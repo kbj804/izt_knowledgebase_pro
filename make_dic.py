03bf5737a22c0f59772ae34f19d46d5904b60f58
kbj804 <kbj804@inzent.com>
2019-12-12 11:03:21 +0900

    maybe pls would be finish pre-processing for wordfile

--- make_dic.py
+++ make_dic.py
@@ -54,45 +54,38 @@ if __name__ == "__main__":
     for j in range(1,len(doc_result.body)):
         doc_body = doc_result.body[j]
         doc_len = len(doc_body)
-        print("DOC ID : ", j)
-        #print('ID = {0}00{1}'.format(str(j),str(i)))
-        #print("DOC LENGTH : ", doc_len)
-        for i in range(doc_len):
-            doc2_len = len(doc_body[i])
-            #print("DOC2 LENGTH : " ,doc2_len)
-            if doc2_len == 1 & doc_len == 1:
+        print('ID = {0}00'.format(str(j)))
+        if doc_len == 1:
+            for i in range(doc_len):
                 # 일반 텍스트 글들 -> 전처리 필요함
-                # clear_doc = filter(None, doc_body[i][0])
                 pre_text = remove_blank(list(doc_body[i][0]))
-
-                # 사전(목차)에 있는 단어인지 비교 후 처리
                 temp_list =[]
                 last_list_value = pre_text[-1]
+                
+                # 사전(목차)에 있는 단어인지 비교 후 처리
                 for subject in pre_text:
                     if subject in list_dic:
                         print(temp_list)
                         temp_list.clear()
                         print("---------------------------------")
                         temp_list.append(subject)
-                    elif subject in last_list_value:
+                    elif subject == last_list_value:
                         temp_list.append(subject)
                         print(temp_list)
                         temp_list.clear()
                     else:
                         temp_list.append(subject)
-                    
-            elif doc2_len == 2 :
-                # 일반적으로 표 형태 2x2
-                    print(doc_body[i])
-
+        else:
+            
+            # 테이블 처리
+            table_list=[]
+            for i in range(len(doc_body)):
+                filed_list=[]
+                for k in range(len(doc_body[i])):
+                    filed_list.append(doc_body[i][k][0])
+                table_list.append(filed_list)
+            print(table_list)
                 
-                
-
-            elif doc2_len > 2:
-                print(doc_body[i])
-                # 2x2를 벗어나는 표 형태
-            else:
-                pass                
 
 
 
